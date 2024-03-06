60141db9b0e5688c530581c97350a7b1dff8ea6c
Yabila <cris.yabimaychi@gmail.com>
2024-03-05 23:12:44 -0600

    Impresión de nombre Yabila

--- src/pruebaEquipo/EjemploPrueba.java
+++ src/pruebaEquipo/EjemploPrueba.java
@@ -6,7 +6,7 @@ public class EjemploPrueba {
 		// TODO Auto-generated method stub
      System.out.println("Hola,¿Cómo te llamas?");
      System.out.println("Hola,me llamo Alba");
-     
-	}
+     System.out.println("Hola, me llamo Yabilá");
+     }
 
 }
