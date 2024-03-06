7d8f3084fe6c7c21f637da2a44d2bcb756a823ee
gabita142003 <gabita142003@gmail>
2024-03-05 20:16:02 -0600

    Impresión de nombre Alba

--- src/pruebaEquipo/EjemploPrueba.java
+++ src/pruebaEquipo/EjemploPrueba.java
@@ -5,6 +5,7 @@ public class EjemploPrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hola,¿Cómo te llamas?");
+     System.out.println("Hola,me llamo Alba");
      
 	}
 
